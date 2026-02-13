1a3bb6838688721c51ff215fc35fb155bf2dd374
Styleiconsai_ <saikumarmsk7799@gmail.com>
2026-02-13 11:34:31 +0530

    Add Grafana installation script

---
+++ monitoring/grafana-install.sh
@@ -0,0 +1,7 @@
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+
+helm install grafana grafana/grafana \
+  --namespace monitoring \
+  --set persistence.enabled=false \
+  --set adminPassword=admin
